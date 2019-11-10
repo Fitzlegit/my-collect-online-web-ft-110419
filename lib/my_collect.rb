0131f8b00991c724e1b7e05a15c068c44067c140
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-10 04:41:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,11 +10,6 @@ def my_collect(array)
   array
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
